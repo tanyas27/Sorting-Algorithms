07bc96e402975e0c5cad7aed807882763f8c4dbc
Tanya Singh <tanyacs27@gmail.com>
2020-01-18 19:28:45 +0530

    Radix sort

--- radixSort.js
+++ radixSort.js
@@ -1,23 +1,18 @@
-function countSort(arr, digits){
-    var sortA = [arr.length];
-    let exp = 10 ** digits ;
-    let maxElatD = arr.reduce((prev,item) => Math.max(prev,item%exp),0 );
+function countSort(arr, n, exp){
+    var sortA = new Array(arr.length);
+    var AuxA = new Array(10).fill(0);
     
-    var AuxA = new Array(maxElatD);
-    for(let i=0; i<=maxElatD ; i++){
-        AuxA[i] =0;
-    }
-    for(let i =0; i <arr.length ; i++)
+    for(let i =0; i <n ; i++)
     {
-        AuxA[arr[i]%exp]++;
+        AuxA[(Math.floor(arr[i]/exp))%10]++;
     }
-    for(let i=1; i<=maxElatD ; i++){
+    for(let i=1; i<10 ; i++){
         AuxA[i] += AuxA[i-1];
     }
-    for(let i=0; i <arr.length ;i++)
+    for(let i=n-1; i>=0 ;i--)
     {
-       sortA[AuxA[arr[i]%exp]-1]= arr[i] ;
-       AuxA[arr[i]%exp]-- ;
+       sortA[AuxA[(Math.floor(arr[i]/exp))%10]-1]= arr[i] ;
+       AuxA[(Math.floor(arr[i]/exp))%10]-- ;
          
     }
     return sortA ;
@@ -25,12 +20,11 @@ function countSort(arr, digits){
 
 function radixSort(arr){
     let max = Math.max(...arr) ;
-    let sortAr = new Array(arr.length);
-    let digits = String(max).split('').length;
-    for(let i=1;i<=digits;i++){
-      sortAr = countSort(arr,i);     
+    let sortAr = arr;
+    for(let exp = 1; Math.floor(max/exp) > 0; exp *= 10){
+      sortAr = countSort(sortAr,sortAr.length,exp);     
     }
-    return sortAr ;
+    return sortAr;
 }
  let arr = [121, 432, 564, 23, 1, 45, 788];
  console.log(radixSort(arr));
